99d53f0ecf2d22614cd37646ad0a9e61a5da207d
a.Cincarevic <acoaco908@gmail.com>
2016-11-19 20:52:00 +0100

    MySQL Databas integrerad

--- app.js
+++ app.js
@@ -31,21 +31,42 @@ g = {}; // all global variables (ex. settings)
   m[x.replace(/\W/g,'')] = require(x);
 });
 
-console.log("All loaded modules", Object.keys(m));
+//console.log("All loaded modules", Object.keys(m));
 
 // constructs g.settings object
 m.settingsConstr();
-
 // loads all classes
 m.classLoader();
 
-console.log("All loaded classes", Object.keys(g.classes));
+//console.log("All loaded classes", Object.keys(g.classes));
 
-// connect to DB
+
+
+
+
+
+// connect to mongoDB
 new g.classes.DB();
+//Connect to MySQL
+/*g.dbinstance = new g.classes.sql();*/
+
+
 
 // start LessWatch
 new g.classes.LessWatch();
-
 // start express server
 new g.classes.Server();
+
+
+
+
+// Usage anywhere:
+// g.dbinstance.connection("SQL",[params]);
+
+
+
+// TestQuery
+/*g.dbinstance.query("SELECT * FROM people WHERE id = ?",[1],function(err,response){
+  console.log("ERROR",err);
+  console.log("Query Resultat:",response);
+});*/
