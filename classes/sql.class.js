072787f2a8cf1d9e7664fae81c88570205878fe3
a.Cincarevic <acoaco908@gmail.com>
2016-11-19 20:49:53 +0100

    MySQL Class

---
+++ classes/sql.class.js
@@ -0,0 +1,42 @@
+//var db = require('mysql');
+
+module.exports = class DbConnection {
+	
+	constructor() {
+
+		this.connect();
+	}
+
+	connect() {
+
+		this.connection = m.mysql.createConnection({
+			host     : '127.0.0.1',
+			port	 : '3306',
+			user     : 'root',
+			password : 'storBIL123',
+			database : 'bilverkstad',
+			timezone: 'utc',
+			dateStrings: true,
+			bigNumberStrings: true,
+			supportBigNumbers: true
+		});
+
+		this.connection.connect(function(err) {
+		    if (err){ 
+				throw err; 
+			}
+			else{
+				console.log("Ansuten till Sql DB");
+				//connection.end();
+			};
+		});
+
+	}
+
+	query(sql,params,callback){
+		this.connection.query(sql,params,callback);
+	}
+
+
+
+}
