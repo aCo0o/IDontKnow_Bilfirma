58c2ea93a24298abdd6061668a15c851ba3df6d3
Hamze Mahmoud <hamzeh__@hotmail.com>
2016-11-20 23:07:33 +0100

    Lagt till SQL querys

--- classes/REST.class.js
+++ classes/REST.class.js
@@ -5,6 +5,7 @@ module.exports = class REST {
   constructor(express) {
     this.settings = s.REST;
     this.DB = new g.classes.DB(); // DB connection & models
+    this.SQL =  new g.classes.sql();
     this.app = express;
 
     this.router();
@@ -18,25 +19,45 @@ module.exports = class REST {
         m.dummyGenerator();
         res.json({"OK": "Ett objekt har blivit skapad och skickat till databas"});
       }
+
       else{
 
-        var model = me.DB.getModel(req.params.model);
-        
-        if (!me[req.method] || !model) {
-          res.sendStatus(404);
-          res.end();
-          return;
-        }
+        // använd sql DB
+        if(req.params.databas == "sql"){
+          
+          var method = req.method+"sql";
+          var params = req.params.modelid || {};
+          var model = req.params.model;
+ 
+          return me[method](req.params.modelid,req.params.model,req,res);
 
-        // combine any data sent in the request body with
-        // any data sent in the request URL
-        var params = req.body || {};
-        params.model = req.params.model;
-        if (req.params.modelid) {
-          params.modelID = req.params.modelid;
-        }
+        }// Eller MongoDB
+        if(req.params.databas == "mongo"){
+          /*return res.json({"OK": "Kopplar till MONGO "});*/
 
-        me[req.method](model, params, req, res);
+
+            var model = me.DB.getModel(req.params.model);
+            
+            if (!me[req.method] || !model) {
+              res.sendStatus(404);
+              res.end();
+              return;
+            }
+
+            // combine any data sent in the request body with
+            // any data sent in the request URL
+            var params = req.body || {};
+            params.model = req.params.model;
+            if (req.params.modelid) {
+              params.modelID = req.params.modelid;
+            }
+
+            me[req.method](model, params, req, res);
+
+          }
+
+
+
       }
     });
   }
@@ -51,6 +72,8 @@ module.exports = class REST {
       if (err) { me.error(err, res); return; }
       res.json(result); // respond with result
     });
+
+    
   }
 
   // READ
@@ -68,6 +91,8 @@ module.exports = class REST {
       if(err){console.log(err)}
         res.json(results)
     })
+
+   
   }
 
   // UPDATE
@@ -79,6 +104,7 @@ module.exports = class REST {
       if (err) { me.error(err, res); return; }
       res.json(result); // respond with result
     });
+
   }
 
   // DELETE
@@ -90,6 +116,94 @@ module.exports = class REST {
       if (err) { me.error(err, res); return; }
       res.json(true); // respond with result
     });
+
+  }
+
+
+  GETsql(id,model,req,res){
+    console.log(id)
+    if(!id)  {
+      this.SQL.query("SELECT * FROM "+model,function(err,response){
+        if(err) res.send("ERROR: " + err);
+        else{
+          console.log("GET - MySQL Resultat:",response);
+          res.json(response);
+        } 
+      });
+    }else {
+      this.SQL.query("SELECT * FROM "+model+" WHERE id = "+id ,function(err,response){
+        if(err) res.send("ERROR: " + err);
+        else{
+          console.log("GET - MySQL Resultat:",response);
+          res.json(response);
+        } 
+      });
+    }
+     
+
+/*
+
+
+
+    if(JSON.stringify(params) == {}){
+      // Get Allt på SQL
+      this.SQL.query("SELECT * FROM "+model,function(err,response){
+        if(err) res.send("ERROR: " + err);
+        else{
+          console.log("GET - MySQL Resultat:",response);
+          res.json(response);
+        } 
+      });
+    }else{
+      // Get Allt på SQL
+    this.SQL.query("SELECT * FROM "+model+ " WHERE id = " + params,function(err,response){
+      if(err) res.send("ERROR: " + err);
+      else{
+        console.log("GET - MySQL Resultat:",response);
+        res.send(response);
+      } 
+    });
+    }
+*/
+    
+  }
+
+  POSTsql(id,model,req,res){
+
+    // INSERT på SQL
+    this.SQL.query("INSERT INTO "+model+" SET ?",req.body,function(err,response){
+      if(err) console.log("ERROR",err);
+      else {
+          console.log("GET - MySQL Resultat:",response);
+          res.send(response);
+      }
+    });
+
+  }
+  PUTsql(id,model,req,res){
+    // UPDATE på SQL
+    this.SQL.query("UPDATE "+model+" SET ? WHERE id = ?",[ req.body, id] ,function(err,response){
+      if(err) console.log("ERROR",err);
+      else {
+        console.log("UPDATE - MySQL Resultat:",response);
+        res.send(response);
+      }
+
+    });
+
+  }
+  DELETEsql(id,model,req,res){
+
+    // DELETE på SQL
+
+    this.SQL.query("DELETE FROM "+model+" WHERE id = ?",id ,function(err,response){
+      if(err) console.log("ERROR",err);
+      else {
+        console.log("DELETE - MySQL Resultat:",response);
+        res.send(response);
+      }
+    });
+    
   }
 
   error(err, res) {
